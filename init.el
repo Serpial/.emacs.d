73cdae6c670eeb867be13343a9ec9d24c20e24e3
serpial <phutchison8@gmail.com>
2018-11-28 19:24:33 +0000

    Added helm and remapped a lot of keys

--- init.el
+++ init.el
@@ -4,7 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (markdown-mode auto-complete use-package))))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (helm-ag helm-swoop helm-descbinds helm d-mode T markdown-mode auto-complete use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -44,14 +47,58 @@
 	:ensure t
 	:init
 	)
-
 (load-theme 'monokai t)
+
+;; Helm stuff
+(use-package helm
+  :ensure t
+  :bind (("M-a" . helm-M-x)
+         ("C-x C-f" . helm-find-files)
+         ("C-x f" . helm-recentf)
+         ("C-SPC" . helm-dabbrev)
+         ("M-y" . helm-show-kill-ring)
+         ("C-x b" . helm-buffers-list))
+  :bind (:map helm-map
+	      ("M-i" . helm-previous-line)
+	      ("M-k" . helm-next-line)
+	      ("M-I" . helm-previous-page)
+	      ("M-K" . helm-next-page)
+	      ("M-h" . helm-beginning-of-buffer)
+	      ("M-H" . helm-end-of-buffer))
+  :config (progn
+	    (setq helm-buffers-fuzzy-matching t)
+            (helm-mode 1)))
+(use-package helm-descbinds
+  :ensure t
+  :bind ("C-h b" . helm-descbinds))
+(use-package helm-files
+  :bind (:map helm-find-files-map
+	      ("M-i" . nil)
+	      ("M-k" . nil)
+	      ("M-I" . nil)
+	      ("M-K" . nil)
+	      ("M-h" . nil)
+	      ("M-H" . nil)))
+(use-package helm-swoop
+  :ensure t
+  :bind (("M-m" . helm-swoop)
+	 ("M-M" . helm-swoop-back-to-last-point))
+  :init
+  (bind-key "M-m" 'helm-swoop-from-isearch isearch-mode-map))
+(use-package helm-ag
+  :ensure helm-ag
+  :bind ("M-p" . helm-projectile-ag)
+  :commands (helm-ag helm-projectile-ag)
+  :init (setq helm-ag-insert-at-point 'symbol
+helm-ag-command-option "--path-to-ignore ~/.agignore"))
 
 ;; Stops the startup screen from showing
 (setq inhibit-startup-screen t)
 
 ;; add line numbers at side of screen
 (global-linum-mode t)
+;;     disable linum mode when in shell
+(add-hook 'shell-mode-hook (lambda () (linum-mode -1))) 
 
 ;; Automatically close braces and stuff
 (electric-pair-mode 1)
@@ -65,8 +112,7 @@
 
 ;; Changes the way the editor looks
 (tool-bar-mode -1) ;; minimal
-(custom-set-variables
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
 
 
 ;; Change the default font size
@@ -85,8 +131,16 @@
 ;;     End of buffer
 (global-set-key (kbd "M-.") 'end-of-buffer)
 (global-unset-key (kbd "M->"))
-
+;;     Change what set mark is
+(global-set-key (kbd "C-M-SPC") 'set-mark-command)
 
 ;; make emacs semi-transparent
 (set-frame-parameter (selected-frame) 'alpha '(92 . 95))
 (add-to-list 'default-frame-alist '(alpha . (92 . 95)))
+
+
+;; Install Extra modes
+;;     D-mode
+(use-package d-mode
+  :ensure t
+  :mode ("\\.d$" . d-mode))
