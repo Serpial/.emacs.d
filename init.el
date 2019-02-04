0d86a7968655671b76e413e7fb6241798e08ced6
serpial <paulhutchison@protonmail.com>
2019-02-04 10:03:26 +0000

    New themee

--- init.el
+++ init.el
@@ -5,9 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages
-   (quote
-    (go-mode T markdown-mode auto-complete use-package))))
+ '(package-selected-packages (quote (go-mode T markdown-mode auto-complete use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -57,11 +55,11 @@
     (global-auto-complete-mode t)
     ))
 
-(use-package monokai-theme
+(use-package klere-theme
         :ensure t
         :init
         )
-(load-theme 'monokai t)
+(load-theme 'klere t)
 
 ;; Function for reopening the file in sudo mode
 (defun er-sudo-edit (&optional arg)
