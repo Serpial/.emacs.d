fea5f65abb899d7e68f7a51b9f1dabd743b4fd75
Serpial <phutchison8@gmail.com>
2024-10-26 17:30:17 +0100

    Update org and stop maximize by default

--- init.el
+++ init.el
@@ -7,9 +7,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-frame-alist '((fullscreen . maximized)))
+;; '(initial-frame-alist '((fullscreen . maximized)))
  '(package-selected-packages
-   '(flycheck neotree all-the-icons diff-hl auto-le dired-sidebar openwith org-plus-contrib use-package)))
+   '(flycheck neotree all-the-icons diff-hl auto-le dired-sidebar openwith org-contrib use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -26,7 +26,6 @@
 ;; Installs melpa and use-package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 (setq package-enable-at-startup nil)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -192,7 +191,7 @@
 ;;; Code:
 (use-package org
   :mode (("\\.org$" . org-mode))
-  :ensure org-plus-contrib
+  :ensure org-contrib
   :bind-keymap ("C-c C-x C-s" . mark-done-and-archive))
 
 ;; Useful Functions
